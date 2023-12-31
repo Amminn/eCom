03126b3078788d401b6b6f18ee167733b87880ea
Amminn <vrb.nnn@gmail.com>
2023-12-31 07:30:13 +0100

    update: add comments to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,25 +21,32 @@ export default function Contact() {
       [e.target.name]: e.target.value
     }))
  }
-
- function validateForm() {
-    let isValid = true;
-    Object.keys(form).forEach(key => {
-      if (form[key].length < 3) {
-        setFormErrors(prevState => ({
-          ...prevState,
-          [key]: true
-        }));
-        isValid = false;
-      } else {
-        setFormErrors(prevState => ({
-          ...prevState,
-          [key]: false
-        }));
-      }
-    });
-    return isValid;
- }
+ function validateForm(form, setFormErrors) {
+  // Initialize the isValid variable as true
+  let isValid = true;
+  // Loop through each key in the form object
+  Object.keys(form).forEach(key => {
+    // Check if the value of the key is less than 3 characters
+    if (form[key].length < 3) {
+      // if it's not valid
+      // Set the form error for the key to true
+      setFormErrors(prevState => ({
+        ...prevState,
+        [key]: true
+      }));
+      // Set isValid to false
+      isValid = false;
+    } else {
+      // Set the form error for the key to false
+      setFormErrors(prevState => ({
+        ...prevState,
+        [key]: false
+      }));
+    }
+  });
+  // Return the isValid value
+  return isValid;
+}
 
  function handleSubmit(e) {
     e.preventDefault();
@@ -57,6 +64,7 @@ export default function Contact() {
  return (
     <div className="container">
       <h1>Contact Us</h1>
+      {/* on submit call handleSubmit */}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="fullName">Full Name</label>
@@ -67,6 +75,7 @@ export default function Contact() {
             value={form.fullName}
             onChange={handleChange}
           />
+          {/* if formErrors.fullName is true display it's error */}
           {formErrors.fullName && <p className="error">Error: Please ensure Full Name has a minimum length of 3 characters.</p>}
         </div>
         <div>
